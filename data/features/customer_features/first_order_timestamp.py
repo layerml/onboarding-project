ad8ed277b05530077eec1aa050884d12c2e34bf6
Burak Ozen <burak@layer.co>
2021-10-27 16:07:46 +0200

    all changes

---
+++ data/features/customer_features/first_order_timestamp.py
@@ -0,0 +1,24 @@
+from typing import Any
+from layer import Dataset
+
+def build_feature(orders_dataset_layer: Dataset("orders_dataset"),customers_dataset_layer: Dataset("customers_dataset")) -> Any:
+    # Convert Layer datasets into pandas data frames
+    orders_df = orders_dataset_layer.to_pandas()
+    customers_df = customers_dataset_layer.to_pandas()
+
+    # We will only take "delivered" orders into consideration during our analysis
+    orders_df = orders_df[orders_df.ORDER_STATUS == "delivered"]
+
+    # Join 2 pandas data frames
+    users_df = orders_df.merge(customers_df, left_on='CUSTOMER_ID', right_on='CUSTOMER_ID', how='left')
+
+    # Add a new column order_rank: For each user, rank the orders with respect to their purchase time
+    users_df["ORDER_RANK"] = users_df.groupby('CUSTOMER_UNIQUE_ID')['ORDER_PURCHASE_TIMESTAMP'].rank(method='first')
+
+    # Filter out only the first orders of users in the dataset
+    users_df = users_df[users_df["ORDER_RANK"] == 1.0].drop(columns=['ORDER_RANK'])
+
+    # Rename columns and select only the columns to be returned
+    first_order_timestamp = users_df.rename(columns={"ORDER_PURCHASE_TIMESTAMP": "FIRST_ORDER_TIMESTAMP"})[['CUSTOMER_UNIQUE_ID', 'FIRST_ORDER_TIMESTAMP']]
+
+    return first_order_timestamp
---
+++ data/features/order_features/days_between_delivery_and_purchase.py
@@ -0,0 +1,14 @@
+from typing import Any
+from layer import Dataset
+
+def build_feature(orders_layer_df: Dataset("orders_dataset")) -> Any:
+    # Convert Layer Dataset into pandas data frame
+    orders_df = orders_layer_df.to_pandas()
+
+    # Compute a new feature: DAYS_BETWEEN_DELIVERY_AND_PURCHASE
+    orders_df["DAYS_BETWEEN_DELIVERY_AND_PURCHASE"] = (orders_df.ORDER_DELIVERED_CARRIER_DATE - orders_df.ORDER_PURCHASE_TIMESTAMP).dt.days
+
+    # Select only columns to be returned
+    days_between_delivery_and_purchase = orders_df[['ORDER_ID', 'DAYS_BETWEEN_DELIVERY_AND_PURCHASE']]
+
+    return days_between_delivery_and_purchase
