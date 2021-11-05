1a5c63d28eab52a63f9b95d5d23d492443325722
Burak Ozen <burak@layer.co>
2021-11-05 16:05:15 +0100

    changes

---
+++ tutorial1_adding_new_datasets_and_features/data/features/order_features/review_score.py
@@ -0,0 +1,19 @@
+from typing import Any
+from layer import Dataset
+import pandas as pd
+
+def build_feature(reviews_layer_df: Dataset("reviews_dataset")) -> Any:
+    # Convert Layer Datasets into pandas dataframes
+    reviews_df = reviews_layer_df.to_pandas()
+
+    # For some reason, there might be multiple reviews for a single order in the data - Only take into account the latest review record
+    # Compute a new feature: LATEST_REVIEW_TIMES
+    reviews_df['LATEST_REVIEW_TIMES'] = reviews_df.groupby(['ORDER_ID'])['REVIEW_ANSWER_TIMESTAMP'].transform('max')
+
+    # Fetch only latest review
+    reviews_df = reviews_df[reviews_df['LATEST_REVIEW_TIMES'] == reviews_df['REVIEW_ANSWER_TIMESTAMP']]
+
+    # Return only ORDER_ID and REVIEW_SCORE columns
+    review_score = reviews_df[['ORDER_ID', 'REVIEW_SCORE']]
+
+    return review_score
